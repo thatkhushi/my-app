82dbe5c4d1c9e1f9aa132ac06318e3c7ca594d72
Khushi <khushisingh4287702@gmail.com>
2022-12-12 18:49:28 +0530

    Just done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import AllRoutes from "./AllRoutes";
 import Navbar from "./components/Navbar/Navbar";
+import AllRoutes from "./AllRoutes";
 
 function App() {
   return (
