8baab397e3a4566e61b82536598515e5a8dc388e
Khushi <khushisingh4287702@gmail.com>
2022-12-19 20:35:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav";
+import { Container } from "@mui/system";
+import Movies from "./Pages/Movies/Movies";
+import Trending from "./Pages/Trending/Trending";
+import Search from "./Pages/Search/Search";
+import Series from "./Pages/Series/Series";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<Trending/>} />
+
+            <Route path="/movies" element={<Movies/>} />
+
+            <Route path="/series" element={<Series/>} />
+
+            <Route path="/search" element={<Search/>} />
+          </Routes>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
